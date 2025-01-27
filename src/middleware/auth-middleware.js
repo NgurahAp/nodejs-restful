e63c80575e2f0a35fbcea671984397931774c078
Arya <ngurahpratama2002@gmail.com>
2025-01-27 09:20:10 +0700

    Done node js restful API

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,21 +1,26 @@
 import { prismaClient } from "../application/database.js";
 
 export const authMiddleware = async (req, res, next) => {
-  const token = req.get("Authorization");
-  if (!token) {
-    res.status(401).json({ errors: "Unauthorized" }).end();
-  } else {
-    const user = await prismaClient.user.findFirst({
-      where: {
-        token: token,
-      },
-    });
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    return res.status(401).json({ errors: "Unauthorized" }).end();
+  }
 
-    if (!user) {
-      res.status(401).json({ errors: "Unauthorized" }).end();
-    } else {
-      req.user = user;
-      next();
-    }
+  const token = authHeader.split(" ")[1]; // Memisahkan "Bearer" dan token
+  if (!token) {
+    return res.status(401).json({ errors: "Unauthorized" }).end();
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      token: token, // Pastikan token disimpan tanpa prefix "Bearer"
+    },
+  });
+
+  if (!user) {
+    return res.status(401).json({ errors: "Unauthorized" }).end();
+  } else {
+    req.user = user;
+    next();
   }
 };
